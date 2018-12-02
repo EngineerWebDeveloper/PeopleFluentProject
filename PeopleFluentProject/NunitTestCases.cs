4c6bcff0449d963c04285ea4c3160074f8ce8601
abc <abc@example.com>
2018-12-02 15:01:56 -0500

    Done with Step 1

---
+++ PeopleFluentProject/NunitTestCases.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleFluentProject
+{
+    [TestFixture]
+    class NunitTestCases
+    {
+
+        [TestCase]
+        public void CheckoutSystem()
+        {
+            Assert.AreEqual(195, ShopCheckoutSystem.CheckoutSystem(new List<string>() { "Apple", "Orange", "Apple", "Orange", "Orange" }, new Dictionary<string, int>() { { "Apple", 60 }, { "Orange", 25 } }));
+
+        }
+    }
+}
---
+++ PeopleFluentProject/ShopCheckoutSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleFluentProject
+{
+    class ShopCheckoutSystem
+    {
+        /// <summary>
+        /// CheckoutSystem: returns the total price based on fruitNames
+        /// </summary>
+        /// <param name="fruitNames">list of fruits to be check out</param>
+        /// <param name="fruitPrices">Contains the price of each fruit</param>
+        /// <returns>the total price</returns>
+
+        public static float CheckoutSystem(List<string> fruitNames, Dictionary<string, int> fruitPrices)
+        {
+            return fruitNames.Select(x => fruitPrices[x]).Sum();
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
